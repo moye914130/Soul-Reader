6b2ec8bb149ad3f3a4e49c12d2cc2397eb0679a1
moye914130 <moye914130@gmail.com>
2025-12-01 00:30:21 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// [https://vitejs.dev/config/](https://vitejs.dev/config/)
+export default defineConfig({
+  plugins: [react()],
+  base: "/Soul-Reader/", // <--- 加上這一行 (前後都要有斜線)
+})
